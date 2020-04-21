73a8dcbd3c33fd9364c3fc0860da5d0f35d0a845
DecoSolar <63842785+DecoSolar@users.noreply.github.com>
2020-04-21 19:33:15 +0200

    Сумма элементов столбцов не содержащих отрицательных чисел

--- 3/3_16.py
+++ 3/3_16.py
@@ -1,5 +1,20 @@
-chisla = list(map(int, input("Введите числа: ").split(',')))
-summ = 0
-for i in range(len(chisla)):
-    summ += chisla[i]
-print("Сумма введенных чисел равна ", summ)
+import numpy as np
+import random
+
+m = int(input("Введите число строк: "))
+n = int(input("Введите число столбцов: "))
+matrix = np.zeros((m,n))
+for i in range(m):
+    for j in range(n):
+        matrix[i][j] = random.randint(-100, 100)
+    print(matrix[i])
+k = np.zeros(n)
+for j in range(n):
+    for i in range(m):
+        if matrix[i][j] < 0:
+            k[j] = 0
+            break
+        else:
+            k[j] += matrix[i][j]
+print("\n", k)
+    
