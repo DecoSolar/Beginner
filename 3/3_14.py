aca6d5413960c82083df51c5834b0c7a24f9d98b
DecoSolar <63842785+DecoSolar@users.noreply.github.com>
2020-04-21 18:31:24 +0200

    Сортировка по номеру телефона в файле

--- 3/3_14.py
+++ 3/3_14.py
@@ -1,26 +1,6 @@
-import numpy as np
-
-def sort(mas, m, n):
-    for i in range(1, m):
-        for j in range(n):
-            if mas[i-1][j] > mas[i][j]:
-                for j in range(n):
-                    k = mas[i-1][j]
-                    mas[i-1][j] = mas[i][j]
-                    mas[i][j] = k
-                break
-    if m == 1:
-        return mas
-    else:
-        return sort(mas, m-1, n)
-        
-
 with open('Dan.txt') as file:
     f = file.readlines()
-mass = np.zeros((len(f), 11))
+    f.sort(key = lambda line: int(line.split(' ')[0]))
 for i in range(len(f)):
-    for j in range(11):
-        mass[i][j] = f[i][j]
-mass = sort(mass, len(f), 11)
-print(mass)
+    print(f[i])
 file.close()
